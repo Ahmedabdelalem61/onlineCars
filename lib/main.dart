5192cbbcc2a044ee6f68694c85039f8f3df0cb63
ahmed abdelalem <ahmedabdelalem61>
2021-05-19 02:02:53 +0200

    search filtration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:online_khalidcars/stateful/filtration_screen.dart';
+import 'package:online_khalidcars/stateless/send.dart';
+
+import 'stateful/srearch_filtration_dialog.dart';
 
 void main() {
-  runApp(MaterialApp(home: FiltrationScreen()));
+  runApp(MaterialApp(home: SearchFiltration()));
 }
